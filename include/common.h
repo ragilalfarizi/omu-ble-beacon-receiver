a60701f518fbd51179ce1041eea24c74237f467a
ragilalfarizi <alfariziragill@gmail.com>
2025-09-03 11:28:18 +0700

    feat: add X series of Device Identity on ListID

--- include/common.h
+++ include/common.h
@@ -9,7 +9,7 @@
 #define RS485_TX_PIN        19
 #define PROTOCOL_DEFAULT_ID 0xB0
 
-#define FIRMWARE_VERSION "v1.3.1-250602"
+#define FIRMWARE_VERSION "v1.3.2-250905"
 
 struct GPSData_t {
   float longitude;
