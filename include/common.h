3283083425ad33863b103f22042aaa87648504cf
ragilalfarizi <alfariziragill@gmail.com>
2025-09-15 18:05:57 +0700

    bug: changing rs485 to be 9600 from 115200

--- include/common.h
+++ include/common.h
@@ -4,12 +4,12 @@
 
 #define BEACON_DATA_CHAR_SIZE 19
 
-#define RS485_BAUD_RATE     115200
+#define RS485_BAUD_RATE     9600
 #define RS485_RX_PIN        18
 #define RS485_TX_PIN        19
 #define PROTOCOL_DEFAULT_ID 0xB0
 
-#define FIRMWARE_VERSION "v1.3.2-250905"
+#define FIRMWARE_VERSION "v1.3.3-250915"
 
 struct GPSData_t {
   float longitude;
