9bdc02c1252ed54504f1c964ebfa620dfda97d9a
ragilalfarizi <alfariziragill@gmail.com>
2025-09-15 18:05:57 +0700

    bug: changing rs485 to be 9600 from 115200

--- include/common.h
+++ include/common.h
@@ -4,7 +4,7 @@
 
 #define BEACON_DATA_CHAR_SIZE 19
 
-#define RS485_BAUD_RATE     115200
+#define RS485_BAUD_RATE     9600
 #define RS485_RX_PIN        18
 #define RS485_TX_PIN        19
 #define PROTOCOL_DEFAULT_ID 0xB0
